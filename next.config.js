6a0114e50f7851c98993759bce85aa048759df01
Niklas Wilhelm <niklas.wilhelm@partner.bmw.de>
2023-06-09 11:15:46 +0200

    Test vulnerability scan

--- next.config.js
+++ next.config.js
@@ -1,5 +1,31 @@
 const nextConfig = {
     reactStrictMode: true,
+    poweredByHeader: false,
+    async headers() {
+        return [
+            {
+                source: '/(.*)',
+                headers: [
+                    {
+                        key: 'X-Frame-Options',
+                        value: 'DENY',
+                    },
+                    {
+                        key: 'Content-Security-Policy',
+                        value: "default-src 'self'; script-src 'self' 'unsafe-eval' 'unsafe-inline'; style-src 'self' 'unsafe-inline'",
+                    },
+                    {
+                        key: 'X-Content-Type-Options',
+                        value: 'nosniff',
+                    },
+                    {
+                        key: 'Permissions-Policy',
+                        value: 'camera=(); battery=(); geolocation=(); microphone=()',
+                    },
+                ],
+            },
+        ];
+    },
 };
 
 module.exports = nextConfig;
