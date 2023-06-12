f29f3b4aa31e65a2a81e8f707bf94fd6922689a0
Niklas Wilhelm <niklas.wilhelm@partner.bmw.de>
2023-06-12 13:54:18 +0200

    Add project id to k6 stress test workflow

--- __tests__/k6/stress-test.js
+++ __tests__/k6/stress-test.js
@@ -8,7 +8,7 @@ export const options = {
         { duration: '1m', target: 0 },
     ],
     thresholds: {
-        http_req_duration: ['p(95)<500'],
+        http_req_duration: ['p(95)<750'],
         checks: ['rate>0.99'],
     },
 };
