1c1d80b7136028410b97c176aa9d7ceed61a3726
Niklas Wilhelm <niklas.wilhelm@partner.bmw.de>
2023-06-15 21:52:12 +0200

    Set up Content-Security-Policy header

--- next.config.js
+++ next.config.js
@@ -11,16 +11,12 @@ const nextConfig = {
                         value: 'DENY',
                     },
                     {
-                        key: 'Content-Security-Policy',
-                        value: "default-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval'; child-src 'none'; style-src 'self' 'unsafe-inline'; font-src 'self'",
-                    },
-                    {
                         key: 'X-Content-Type-Options',
                         value: 'nosniff',
                     },
                     {
                         key: 'Permissions-Policy',
-                        value: 'camera=(); battery=(); geolocation=(); microphone=()',
+                        value: 'camera=(), geolocation=(), microphone=()',
                     },
                 ],
             },
