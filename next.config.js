e4450992f1f66f6d1af995ef9f302f027287e9bc
Niklas Wilhelm <niklas.wilhelm@partner.bmw.de>
2023-06-16 08:27:04 +0200

    Set up security headers for Vercel deployment

--- next.config.js
+++ next.config.js
@@ -1,27 +1,6 @@
 const nextConfig = {
     reactStrictMode: true,
     poweredByHeader: false,
-    async headers() {
-        return [
-            {
-                source: '/(.*)',
-                headers: [
-                    {
-                        key: 'X-Frame-Options',
-                        value: 'DENY',
-                    },
-                    {
-                        key: 'X-Content-Type-Options',
-                        value: 'nosniff',
-                    },
-                    {
-                        key: 'Permissions-Policy',
-                        value: 'camera=(), geolocation=(), microphone=()',
-                    },
-                ],
-            },
-        ];
-    },
 };
 
 module.exports = nextConfig;
